5e189f6cf6b13962d727257ad44201ce7dba1e00
Peter <31785812+wangrx33@users.noreply.github.com>
2017-10-08 11:17:38 +0800

    code

---
+++ lab1/lab1/app/src/main/java/com/example/peter/lab1/lab1.java
@@ -0,0 +1,13 @@
+package com.example.peter.lab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class lab1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab1);
+    }
+}
